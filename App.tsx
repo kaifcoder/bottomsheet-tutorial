6e15141a0885864d79696f9cd6eb54f6543ffc9d
Mohd Kaif <kaifmohd2014@gmail.com>
2022-08-28 02:32:35 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useRef, useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
+  const sheetref = useRef<BottomSheet>(null);
+  const [isopen, setIsOpen] = useState(true);
+
+  const snapPoints = ["40%", "80%"];
+  const handlesnappress = useCallback((index: number) => {
+    sheetref.current?.snapToIndex(index);
+    setIsOpen(true);
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => handlesnappress(0)}>
+          <Text>Open</Text>
+        </TouchableOpacity>
+
+        <BottomSheet
+          ref={sheetref}
+          snapPoints={snapPoints}
+          enablePanDownToClose={true}
+          onClose={() => setIsOpen(false)}
+        >
+          <BottomSheetView>
+            <Text>Hello</Text>
+          </BottomSheetView>
+        </BottomSheet>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
